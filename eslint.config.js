b894a9c710c6d2ccc4049fb38024c8a822a65c6f
cskim <tantagain@gmail.com>
2026-02-07 11:08:39 +0000

    fix: resolve lint errors (ignore storybook-static, allow storybook react import)

--- eslint.config.js
+++ eslint.config.js
@@ -8,22 +8,27 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([globalIgnores(['dist']), {
-  files: ['**/*.{ts,tsx}'],
-  extends: [
-    js.configs.recommended,
-    tseslint.configs.recommended,
-    reactHooks.configs.flat.recommended,
-    reactRefresh.configs.vite,
-  ],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-  },
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+export default defineConfig([
+  globalIgnores(['dist', 'storybook-static']),
+  ...storybook.configs["flat/recommended"],
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
     ],
-  },
-}, ...storybook.configs["flat/recommended"]])
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'storybook/no-renderer-packages': 'off',
+    },
+  }
+])
